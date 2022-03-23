dd543b62d3159a19f28cc8b4003d5ca0cfc2b8d3
Kamil Fryszkowski <kamil.fryszkowski@pw.edu.pl>
2022-03-23 11:53:58 +0100

    Dodanie rozpoznawania trybu do readArguments() w main.c

--- C/Program/main.c
+++ C/Program/main.c
@@ -8,64 +8,99 @@ double high = 1;
 int dec = 10;
 int pairN = 5;
 int *pairs;
+int mode = 0;
 
 int
 readArguments (int argc, char **argv)
 {
-  for (int i = 1; i < argc - 1; i++)
+  if (strcmp (argv[1], "loadMode") == 0)
+  {
+      mode = 1;
+    }
+  if (strcmp (argv[1], "allRandMode") == 0)
     {
-      if (strcmp (argv[i], "-r") == 0)
+      mode = 2;
+    }
+  if (strcmp (argv[1], "randWeightMode") == 0)
+    {
+      mode = 3;
+    }
+  if (strcmp (argv[1], "conMode") == 0)
+    {
+      mode = 4;
+    }
+
+  /*if (mode == 1)
+    {
+      //Tutaj wyszukiwanie flag dla loadmode
+      return 0;
+    }
+    To zostanie odkomentowane, kiedy napisze w środku potrzebne komendy
+    Tymczasowo, dla sprawdzenia poprawności danych, poniżej dodany jest warunek
+    || mode == 1*/
+
+  if (mode == 1 || mode == 2 || mode == 3 || mode == 4)
+    {
+      for (int i = 2; i < argc - 1; i++)
 	{
-	  if (atoi (argv[i + 1]) < 1)
-	    continue;
-	  rows = atoi (argv[i + 1]);
-	  continue;
-	}
-      if (strcmp (argv[i], "-c") == 0)
-	{
-	  if (atoi (argv[i + 1]) < 1)
-	    continue;
-	  columns = atoi (argv[i + 1]);
-	  continue;
-	}
-      if (strcmp (argv[i], "-low") == 0)
-	{
-	  if (atof (argv[i + 1]) < 0)
-	    continue;
-	  low = atof (argv[i + 1]);
-	  continue;
-	}
-      if (strcmp (argv[i], "-high") == 0)
-	{
-	  high = atof (argv[i + 1]);
-	  continue;
-	}
-      if (strcmp (argv[i], "-dec") == 0)
-	{
-	  if (atoi (argv[i + 1]) < 0 || atoi (argv[i + 1]) > 20)
-	    continue;
-	  dec = atoi (argv[i + 1]);
-	  continue;
-	}
-      if (strcmp (argv[i], "-p") == 0)
-	{
-	  if (atoi (argv[i + 1]) < 0)
-	    continue;
-	  *pairs = malloc (sizeof (int) * atoi (argv[i + 1]) * 2);
-	  pairN = atoi (argv[i + 1]);
-	  for (int j = 0; j < (pairN * 2); j++)
+	  if (strcmp (argv[i], "-r") == 0)
 	    {
-	      pairs[j] = atoi (argv[i + 2 + j]);
+	      if (atoi (argv[i + 1]) < 1)
+		continue;
+	      rows = atoi (argv[i + 1]);
+	      continue;
 	    }
-	  continue;
+	  if (strcmp (argv[i], "-c") == 0)
+	    {
+	      if (atoi (argv[i + 1]) < 1)
+		continue;
+	      columns = atoi (argv[i + 1]);
+	      continue;
+	    }
+	  if (strcmp (argv[i], "-low") == 0)
+	    {
+	      if (atof (argv[i + 1]) < 0)
+		continue;
+	      low = atof (argv[i + 1]);
+	      continue;
+	    }
+	  if (strcmp (argv[i], "-high") == 0)
+	    {
+	      high = atof (argv[i + 1]);
+	      continue;
+	    }
+	  if (strcmp (argv[i], "-dec") == 0)
+	    {
+	      if (atoi (argv[i + 1]) < 0 || atoi (argv[i + 1]) > 20)
+		continue;
+	      dec = atoi (argv[i + 1]);
+	      continue;
+	    }
+	  if (strcmp (argv[i], "-p") == 0)
+	    {
+	      if (atoi (argv[i + 1]) < 0)
+		continue;
+	      *pairs = malloc (sizeof (int) * atoi (argv[i + 1]) * 2);
+	      pairN = atoi (argv[i + 1]);
+	      for (int j = 0; j < (pairN * 2); j++)
+		{
+		  pairs[j] = atoi (argv[i + 2 + j]);
+		}
+	      continue;
+	    }
 	}
+
+      if (low > high)
+	{
+	  low = 0;
+	  high = 1;
+	}
+      return 0;
     }
-  if (low > high)
+  if (mode == 0)
     {
-      low = 0;
-      high = 1;
+      return 1;
     }
-  return 0;
 }
 
 
@@ -74,6 +109,7 @@ main (int argc, char **argv)
 {
   if (readArguments (argc, argv) == 0)
     {
+      printf ("mode = %d\n", mode);
       printf ("rows = %d\n", rows);
       printf ("columns = %d\n", columns);
       printf ("dec = %d\n", dec);
