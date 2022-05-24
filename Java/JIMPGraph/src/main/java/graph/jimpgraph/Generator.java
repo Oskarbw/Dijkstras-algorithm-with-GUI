c15b4734dbb7afd551326c07034e3b7069660785
Kamil <k.fryszkowski.02@gmail.com>
2022-05-24 11:35:54 +0200

    Dodanie do generator.java przerobionej na nasze potrzeby procedury findNeighbours zaczerpniętej z projektu w C

--- Java/JIMPGraph/src/main/java/graph/jimpgraph/Generator.java
+++ Java/JIMPGraph/src/main/java/graph/jimpgraph/Generator.java
@@ -1,5 +1,32 @@
 package graph.jimpgraph;
 
-public class Generator {
+import java.util.Random;
 
+public class Generator {
+    void findNeighbours(Graph graph){
+        for(int vertexCount=0; vertexCount < graph.columns*graph.rows; vertexCount++) {
+            int currentNumOfNeighbours = 0;
+            Random rand = new Random();
+            if (vertexCount % graph.columns != 0) {
+                graph.vertex[vertexCount][currentNumOfNeighbours] = (vertexCount - 1);
+                graph.weight[vertexCount][currentNumOfNeighbours] = rand.nextDouble() * (graph.max - graph.min) + graph.min;
+                currentNumOfNeighbours++;
+            }
+            if (vertexCount % graph.columns != (graph.columns - 1)) {
+                graph.vertex[vertexCount][currentNumOfNeighbours] = (vertexCount + 1);
+                graph.weight[vertexCount][currentNumOfNeighbours] = rand.nextDouble() * (graph.max - graph.min) + graph.min;
+                currentNumOfNeighbours++;
+            }
+            if (vertexCount >= graph.columns) {
+                graph.vertex[vertexCount][currentNumOfNeighbours] = (vertexCount - graph.columns);
+                graph.weight[vertexCount][currentNumOfNeighbours] = rand.nextDouble() * (graph.max - graph.min) + graph.min;
+                currentNumOfNeighbours++;
+            }
+
+            if(vertexCount < (graph.rows * graph.columns) - graph.columns){
+                graph.vertex[vertexCount][currentNumOfNeighbours] = (vertexCount + graph.columns);
+                graph.weight[vertexCount][currentNumOfNeighbours] = rand.nextDouble() * (graph.max - graph.min) + graph.min;
+            }
+        }
+    }
 }
